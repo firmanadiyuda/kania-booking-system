f7522a351b512d52814adb919fb59fc72b82d8ee
Firman Adiyuda <firmanadiyuda@gmail.com>
2023-05-31 18:07:07 +0700

    Create migration table 'busana'

---
+++ database/migrations/2023_05_31_081540_create_busana_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('busanas', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('nama', 100);
+            $table->text('deskripsi');
+            $table->string('tipe', 20); // (gaun, kebaya, jas, basofi, beskap)
+            $table->string('warna', 20);
+            $table->string('ukuran', 5); // (S, M, L1, L2, L3, L4, L5, L6)
+            $table->text('foto');
+            $table->text('video');
+            $table->integer('harga');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('busana');
+    }
+};
